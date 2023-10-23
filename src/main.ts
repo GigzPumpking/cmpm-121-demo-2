1988ef0cb4f37126d79525d7266b04befb36f96b
Gigz <gigzpumpking@gmail.com>
2023-10-23 12:35:05 -0700

    Steps 1 and 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+const app = document.querySelector("#app")!;
 
 const gameName = "Sticker Power";
 
@@ -9,3 +9,64 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256;
+const ctx = canvas.getContext("2d")!;
+const RECT_X = 0;
+const RECT_Y = 0;
+const RECT_WIDTH = 256;
+const RECT_HEIGHT = 256;
+ctx.fillStyle = "beige";        
+ctx.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+app.append(ctx.canvas);
+
+// implement drawing with mouse
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+canvas.addEventListener("mousedown", (event) => {
+    x = event.offsetX;
+    y = event.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+    if (isDrawing) {
+        drawLine(ctx, x, y, event.offsetX, event.offsetY);
+        x = event.offsetX;
+        y = event.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (event) => {
+    if (isDrawing) {
+        drawLine(ctx, x, y, event.offsetX, event.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
+
+function drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+function clearCanvas() {
+    ctx.clearRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT); 
+    ctx.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+}
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+clearButton.addEventListener("click", clearCanvas);
+app.append(clearButton);
