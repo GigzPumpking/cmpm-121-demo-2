9243962a8046b9d4312a6f9fcf8698b970d6cd23
Gigz <gigzpumpking@gmail.com>
2023-10-27 02:41:17 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import "./style.css";
 const app = document.querySelector("#app")!;
 
 const lineBreak = document.createElement("br");
-const initial = 0;
-const next = 1;
-const prev = -1;
 
 const gameName = "Sticker Power";
 
@@ -32,12 +29,32 @@ app.append(ctx.canvas);
 let isDrawing = false;
 let x = 0;
 let y = 0;
+let newX = 0;
+let newY = 0;
 
-// Declare an array of array of xy coordinates to store the beginning x and y coordinates and end of each line
+// Create a class to represent line segments
+class LineSegment {
+  constructor(
+    private x1: number,
+    private y1: number,
+    private x2: number,
+    private y2: number
+  ) {}
 
-let points: [number, number][] = [];
-const displayPoints: [number, number][][] = [];
-let redoPoints: [number, number][][] = [];
+  display(context: CanvasRenderingContext2D) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(this.x1, this.y1);
+    context.lineTo(this.x2, this.y2);
+    context.stroke();
+    context.closePath();
+  }
+}
+
+let singleSegments: LineSegment[] = [];
+const displaySegments: LineSegment[][] = [];
+let redoSegments: LineSegment[][] = [];
 
 const drawingChange = new Event("drawing-changed");
 
@@ -45,18 +62,26 @@ canvas.addEventListener("mousedown", (event) => {
   x = event.offsetX;
   y = event.offsetY;
   isDrawing = true;
-  points = [];
-  redoPoints = [];
-  displayPoints.push(points);
-  points.push([x, y]);
+  singleSegments = [];
+  redoSegments = [];
+  displaySegments.push(singleSegments);
+  singleSegments.push(new LineSegment(x, y, x, y));
   canvas.dispatchEvent(drawingChange);
 });
 
 canvas.addEventListener("mousemove", (event) => {
   if (isDrawing) {
-    x = event.offsetX;
-    y = event.offsetY;
-    points.push([x, y]);
+    newX = event.offsetX;
+    newY = event.offsetY;
+
+    const currentSegment = new LineSegment(x, y, newX, newY);
+
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    singleSegments.push(currentSegment);
+
+    x = newX;
+    y = newY;
+
     canvas.dispatchEvent(drawingChange);
   }
 });
@@ -64,27 +89,13 @@ canvas.addEventListener("mousemove", (event) => {
 canvas.addEventListener("mouseup", (event) => {
   if (isDrawing) {
     isDrawing = false;
-    points.push([event.offsetX, event.offsetY]);
+    newX = event.offsetX;
+    newY = event.offsetY;
+
+    singleSegments.push(new LineSegment(x, y, newX, newY));
     canvas.dispatchEvent(drawingChange);
-    points = [];
   }
 });
-
-function drawLine(
-  context: CanvasRenderingContext2D,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number
-) {
-  context.beginPath();
-  context.strokeStyle = "black";
-  context.lineWidth = 1;
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
-}
 
 function clearCanvas() {
   ctx.clearRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
@@ -101,9 +112,9 @@ app.append(clearButton);
 const redoButton = document.createElement("button");
 redoButton.innerHTML = "Redo";
 redoButton.addEventListener("click", () => {
-  if (redoPoints.length) {
-    const lastLine = redoPoints.pop();
-    displayPoints.push(lastLine!);
+  if (redoSegments.length) {
+    const lastLine = redoSegments.pop();
+    displaySegments.push(lastLine!);
     if (lastLine) {
       canvas.dispatchEvent(drawingChange);
     }
@@ -114,9 +125,9 @@ app.append(redoButton);
 const undoButton = document.createElement("button");
 undoButton.innerHTML = "Undo";
 undoButton.addEventListener("click", () => {
-  if (displayPoints.length) {
-    const lastLine = displayPoints.pop();
-    redoPoints.push(lastLine!);
+  if (displaySegments.length) {
+    const lastLine = displaySegments.pop();
+    redoSegments.push(lastLine!);
     if (lastLine) {
       canvas.dispatchEvent(drawingChange);
     }
@@ -127,15 +138,9 @@ app.append(undoButton);
 canvas.addEventListener("drawing-changed", () => {
   clearCanvas();
 
-  for (const line of displayPoints) {
-    for (let i = 0; i < line.length + prev; i++) {
-      drawLine(
-        ctx,
-        line[i][initial],
-        line[i][next],
-        line[i + next][initial],
-        line[i + next][next]
-      );
+  for (const segment of displaySegments) {
+    for (const line of segment) {
+      line.display(ctx);
     }
   }
 });
